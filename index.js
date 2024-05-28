6c7b95fc1dabead8ba369b3cc86d70c45fdd37b1
imnamanmodi1 <modi.naman14@gmail.com>
2024-05-28 22:16:26 +0530

    BG Color changed

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 export const applyCustomCode = (externalCodeSetup) => {
+  // call custom code api here - test
+  externalCodeSetup.cssApi.addGlobalStyle("menuLabelStyle", {
+    color: "#fff",
+  });
+
   externalCodeSetup.cssApi.addGlobalStyle("container", {
     backgroundColor: "#030f2d",
   });
